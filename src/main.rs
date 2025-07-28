575ece8ee1d9c120e07faa9f40d55ecb99cf7d95
Tim Swast <tswast@gmail.com>
2025-07-28 17:57:23 -0500

    add command-line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,52 @@
+use std::process;
 
 mod calculator;
 mod cli;
 
+
+fn print_usage_and_exit(command: String, error: String) -> ! {
+    eprintln!("{}\n", error);
+    eprintln!("Usage:");
+    eprintln!("\t{} x y z", command);
+    process::exit(-1);
+}
+
+
+fn parse_args(mut args: std::env::Args) -> calculator::Coord {
+    let command = match args.next() {
+        Some(value) => value,
+        None => "mcchunk".to_string(),
+    };
+
+    let x: i32 = match args.next() {
+        Some(value ) => match value.parse() {
+            Ok(x) => x,
+            Err(err) => print_usage_and_exit(command, format!("Expected x to be an integer, but got {}:\n{}", value, err)),
+        },
+        None => print_usage_and_exit(command, "Expected x, but missing".to_string()),
+    };
+
+    let y: i32 = match args.next() {
+        Some(value ) => match value.parse() {
+            Ok(y) => y,
+            Err(err) => print_usage_and_exit(command, format!("Expected y to be an integer, but got {}:\n{}", value, err)),
+        },
+        None => print_usage_and_exit(command, "Expected y, but missing".to_string()),
+    };
+
+    let z: i32 = match args.next() {
+        Some(value ) => match value.parse() {
+            Ok(z) => z,
+            Err(err) => print_usage_and_exit(command, format!("Expected z to be an integer, but got {}:\n{}", value, err)),
+        },
+        None => print_usage_and_exit(command, "Expected z, but missing".to_string()),
+    };
+
+    calculator::Coord { x: x, y: y, z: z }
+}
+
 fn main() {
-    cli::print_chunk_corners(calculator::Coord { x: -17, y: 2, z: -37 })
+    let args = std::env::args();
+    let pos = parse_args(args);
+    cli::print_chunk_corners(pos);
 }
